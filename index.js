4a9c47e4810687a8ebe9457c6dcbccbd6832a7e8
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2018-06-13 11:14:31 -0700

    Add functions to extract tag hash

--- index.js
+++ index.js
@@ -14,26 +14,30 @@ if (!folder) {
 
 const options = { cwd: folder, stdio: 'inherit' };
 
+// Removes the \n from the stringified buffer
+const extractHash = buffer => {
+  const arr = buffer.toString('utf8').split('\n');
+  return arr[0];
+};
+
 /* 
  * Function fetches the tags off the repository and then pulls
  * the latest tag, rather than the current HEAD of master.
  */
 
 const checkoutLatestTag = () => {
-  console.log('Checking out latest tag');
+  console.log('Checking out latest tag...');
+  // Make sure to fetch the tags to pull the latest
   spawnSync('git', ['fetch', '--tags'], options);
+
+  // Pull the latest tag from the repository
   const pullTag = spawnSync('git', ['rev-list', '--tags', '--max-count=1'], {
     cwd: folder,
   });
-  const tagHash = pullTag.output[1].toString('utf8');
-  console.log(tagHash);
-  const describeTag = spawnSync(
-    'git',
-    ['describe', '--tags', tagHash],
-    options,
-  );
-  // const tagName = describeTag.output[1].toString('utf8');
-  console.log(describeTag.output);
+  const tagHash = extractHash(pullTag.output[1]);
+
+  // Checkout the local repo to the latest tag
+  spawnSync('git', ['checkout', tagHash], { cwd: folder });
 };
 
 /*
@@ -42,6 +46,9 @@ const checkoutLatestTag = () => {
  */
 
 const setupParticle = () => {
+  // This function must complete before the subsequent installs can be ran.
+  checkoutLatestTag();
+
   spawn('rm', ['-rf', '.git/'], options);
   console.log('Running Particle dependency installation...');
   spawnSync('npm', ['install'], options);
@@ -50,4 +57,4 @@ const setupParticle = () => {
 };
 
 console.log('Cloning Particle repo...');
-clone('https://github.com/phase2/particle', folder, {}, checkoutLatestTag);
+clone('https://github.com/phase2/particle', folder, {}, setupParticle);
